4ea7ec98ad1b490b3a031f183dab0efc2feaa1d7
Mayowa <mayowaadegbola.r@gmail.com>
2016-05-17 16:52:57 +0100

    Add search, delete and edit methods

--- NotesApp.js
+++ NotesApp.js
@@ -14,18 +14,37 @@ function NoteApplication (author) {
 	}
 
 	this.listNotes = function(){
-        if(this.noteArray.length < 1){
+        if (this.noteArray.length < 1){
             console.log("There are no notes in the collection.");
         }
-        else{for(let i = 0; i < this.notes.length; i++){
+        else { 
+        	for(let i = 0; i < this.notes.length; i++) {
                 console.log("\nNote ID: " + this.notes[i]);
                 console.log(this.notes[i]);
                 console.log("\n\nBy Author " + this.author + "\n");
-         } 
+            }
+        }
+    } 
 
     this.get = function(note_id) {
       return "Note ID: " + this.notes[note_id];
     }
+
+    this.search = function(search_text){
+    	if (this.notes.includes(search_text)){
+        	return "Note ID: " + this.note_id + "\n" + note_content + "\n\n" + "By Author " + this.author;
+    	} else {
+        return "search text not found"
+    }
+
+    this.delete = function(note_id){
+    	delete this.notes[note_id]
+	}
+
+
+	this.edit = function(note_id, new_content){
+    	this.notes[note_id] = new_content;
+	}
 
     
 }
